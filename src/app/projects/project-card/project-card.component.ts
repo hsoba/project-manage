64aa93d63dc2ecc405fc7968072c06903226536f
H-Soba Vincent <hsobav@gmail.com>
2023-05-31 11:57:46 -0400

    Lab 9 completed - adding project card reusable component

---
+++ src/app/projects/project-card/project-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Project } from '../shared/project.model';
+
+@Component({
+  selector: 'app-project-card',
+  templateUrl: './project-card.component.html',
+  styleUrls: ['./project-card.component.css']
+})
+export class ProjectCardComponent {
+  @Input()
+  project!: Project;
+}
--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsContainerComponent } from './projects-container/projects-container.component';
 import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectCardComponent } from './project-card/project-card.component';
 
 
 @NgModule({
   declarations: [
     ProjectsContainerComponent,
-    ProjectListComponent
+    ProjectListComponent,
+    ProjectCardComponent
   ],
   imports: [
     CommonModule,
